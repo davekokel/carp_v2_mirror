3c75ea64822b38671eac97c4a5ea1b0c80227be8
davekokel <dave.kokel@gmail.com>
2025-09-25 16:46:22 -0700

    Ping redeploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.set_page_config(page_title="ping", layout="wide")
 st.title("✅ Streamlit is rendering")
 st.write("If you can read this, the runtime is alive and the Main file path is correct.")
+# ping
