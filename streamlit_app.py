dafa8ebd1ba575783371fc2d2b311c2093f7b3e2
davekokel <dave.kokel@gmail.com>
2025-09-25 16:58:09 -0700

    Hello world

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
-# streamlit_app.py
 import streamlit as st
 st.set_page_config(page_title="ping", layout="wide")
-st.title("✅ Streamlit is rendering")
-st.write("If you can read this, the runtime is alive and the Main file path is correct.")
-# ping
+st.write("✅ Streamlit is rendering")
