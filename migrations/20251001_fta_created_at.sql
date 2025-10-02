8664fd6d499dc65d43c67f06c05cf53487232071
davekokel <dave.kokel@gmail.com>
2025-10-02 09:27:07 -0700

    migrations: remove old non-dashed filenames; keep canonical dashed names only

--- migrations/20251001_fta_created_at.sql
+++
@@ -1,9 +0,0 @@
--- 20251001_fta_created_at.sql
--- Add created_at to link table (if you want audit timestamps)
-
-begin;
-
-alter table public.fish_transgene_alleles
-  add column if not exists created_at timestamptz not null default now();
-
-commit;
