b4307f9a794b1fb250f96e16c3907ca10eebd85e
davekokel <dave.kokel@gmail.com>
2025-10-11 21:01:21 -0700

    migrations: indexes + NOT VALID FKs on core joins (cross_instances, memberships)

---
+++ supabase/migrations/20251011210121_indexes_and_fks_core.sql
@@ -0,0 +1,27 @@
+-- indexes first (safe)
+create index if not exists ix_cross_instances_cross_id   on public.cross_instances(cross_id);
+create index if not exists ix_cross_instances_mother_id  on public.cross_instances(mother_tank_id);
+create index if not exists ix_cross_instances_father_id  on public.cross_instances(father_tank_id);
+
+create index if not exists ix_ftm_fish        on public.fish_tank_memberships(fish_id);
+create index if not exists ix_ftm_container   on public.fish_tank_memberships(container_id);
+create index if not exists ix_ftm_left_null   on public.fish_tank_memberships(left_at);
+
+-- add FKs as NOT VALID so we don't break if there are orphans
+do $$
+begin
+  if not exists (select 1 from pg_constraint where conname='fk_ci_cross') then
+    alter table public.cross_instances
+      add constraint fk_ci_cross foreign key (cross_id) references public.crosses(id_uuid) NOT VALID;
+  end if;
+
+  if not exists (select 1 from pg_constraint where conname='fk_ci_mother_container') then
+    alter table public.cross_instances
+      add constraint fk_ci_mother_container foreign key (mother_tank_id) references public.containers(id_uuid) NOT VALID;
+  end if;
+
+  if not exists (select 1 from pg_constraint where conname='fk_ci_father_container') then
+    alter table public.cross_instances
+      add constraint fk_ci_father_container foreign key (father_tank_id) references public.containers(id_uuid) NOT VALID;
+  end if;
+end$$;
