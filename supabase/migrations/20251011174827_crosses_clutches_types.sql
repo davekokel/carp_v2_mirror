5e30a77efdabab144fbe8f0047e669f538e7554b
davekokel <dave.kokel@gmail.com>
2025-10-11 17:48:49 -0700

    migrations: enum types for crosses/clutches (staging)

---
+++ supabase/migrations/20251011174827_crosses_clutches_types.sql
@@ -0,0 +1,10 @@
+DO $$ BEGIN
+IF NOT EXISTS (
+  SELECT 1
+  FROM pg_type t
+  JOIN pg_namespace n ON n.oid=t.typnamespace
+  WHERE t.typname='cross_plan_status' AND n.nspname='public'
+) THEN
+  CREATE TYPE public.cross_plan_status AS ENUM ('planned', 'canceled', 'executed');
+END IF;
+END$$;
