f2cebf96d03aab58a152862ddfd491adadcc58e5
davekokel <dave.kokel@gmail.com>
2025-10-14 23:32:00 -0700

    migrations: replace baseline with clean schema dump (v3)

---
+++ supabase/migrations/00000000_baseline_v3.sql
@@ -0,0 +1,41 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict 7qUgBDYSfo7ywPXg6mAdncjKNovnJdwMjq0ld8FWDHSu6XkHiBIPicOMqRq6mP1
+
+-- Dumped from database version 16.10 (Homebrew)
+-- Dumped by pg_dump version 18.0
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA public;
+
+
+--
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON SCHEMA public IS 'standard public schema';
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict 7qUgBDYSfo7ywPXg6mAdncjKNovnJdwMjq0ld8FWDHSu6XkHiBIPicOMqRq6mP1
+
