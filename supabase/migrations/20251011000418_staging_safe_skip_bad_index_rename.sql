2b6c405943f8fbb3a50e45da3121fda7c346a06b
davekokel <dave.kokel@gmail.com>
2025-10-14 19:51:03 -0700

    migrations: make index rename idempotent in 20251011000418_staging_safe_skip_bad_index_rename

--- supabase/migrations/20251011000418_staging_safe_skip_bad_index_rename.sql
+++ supabase/migrations/20251011000418_staging_safe_skip_bad_index_rename.sql
@@ -1,6 +1,11 @@
+-- Idempotent: only rename if old exists and new does not
 DO $$
 BEGIN
-  IF to_regclass('public.idx_fish_transgene_alles_base_allele') IS NOT NULL THEN
-    EXECUTE 'ALTER INDEX IF EXISTS public.idx_fish_transgene_alles_base_allele RENAME TO idx_fish_transgene_alleles_base_allele';
+  IF to_regclass('public.idx_fish_transgene_alles_base_allele') IS NOT NULL
+     AND to_regclass('public.idx_fish_transgene_alleles_base_allele') IS NULL THEN
+    EXECUTE 'ALTER INDEX public.idx_fish_transgene_alles_base_allele RENAME TO idx_fish_transgene_alleles_base_allele';
+  ELSE
+    RAISE NOTICE 'index rename skipped (already renamed or not present)';
   END IF;
-END$$;
+END
+$$ LANGUAGE plpgsql;
