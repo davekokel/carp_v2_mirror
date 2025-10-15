3a6b54c3995c6dd855465e59d678d93e92dfc5e1
davekokel <dave.kokel@gmail.com>
2025-10-15 00:36:41 -0700

    migrations: add role guards before baseline; restore baseline header

---
+++ supabase/migrations/000000015_role_guards.sql
@@ -0,0 +1,10 @@
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname='app_rw') THEN
+    CREATE ROLE app_rw;
+  END IF;
+  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname='app_ro') THEN
+    CREATE ROLE app_ro;
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
