27401c48e70f642db3a06ec043af673b47098338
davekokel <dave.kokel@gmail.com>
2025-10-14 23:04:51 -0700

    migrations(baseline): normalize container_status stanza (single DO 94280 ... END 94280 LANGUAGE plpgsql;)

--- supabase/migrations/00000000_baseline_v2.sql
+++ supabase/migrations/00000000_baseline_v2.sql
@@ -12,7 +12,6 @@
 
 --
 -- Name: container_status; Type: TYPE; Schema: public; Owner: -
---
 
 DO $$
 BEGIN
@@ -22,35 +21,11 @@ BEGIN
     JOIN pg_namespace n ON n.oid = t.typnamespace
     WHERE t.typname = 'container_status' AND n.nspname = 'public'
   ) THEN
-DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1 FROM pg_type t
-    JOIN pg_namespace n ON n.oid=t.typnamespace
-    WHERE t.typname='container_status' AND n.nspname='public'
-  ) THEN
     CREATE TYPE public.container_status AS ENUM ('planned','active','to_kill','retired');
   END IF;
 END
 $$ LANGUAGE plpgsql;
-  END IF;
-END
-$$ LANGUAGE plpgsql;
-  END IF;
-END
-$$ LANGUAGE plpgsql;
-  END IF;
-END
-$$ LANGUAGE plpgsql;
-  END IF;
-END
-$$ LANGUAGE plpgsql;
-  END IF;
-END
-$$ LANGUAGE plpgsql;
 
-
---
 -- Name: cross_plan_status; Type: TYPE; Schema: public; Owner: -
 --
 DO $$
