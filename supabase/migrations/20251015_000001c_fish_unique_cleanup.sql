d4ca9a270e77ac9eab26dc9c33eaa5658df5ccc6
davekokel <dave.kokel@gmail.com>
2025-10-19 22:24:25 -0700

    migrations(core): noop old fish unique cleanup (replaced by fixed cast version)

--- supabase/migrations/20251015_000001c_fish_unique_cleanup.sql
+++ supabase/migrations/20251015_000001c_fish_unique_cleanup.sql
@@ -1,20 +1,2 @@
-DO $$
-DECLARE r record;
-BEGIN
-  FOR r IN
-    SELECT c.conname
-    FROM pg_constraint c
-    JOIN pg_class t ON t.oid = c.conrelid
-    JOIN pg_namespace n ON n.oid = t.relnamespace
-    WHERE n.nspname = 'public'
-      AND t.relname = 'fish'
-      AND c.contype = 'u'
-      AND (
-        SELECT array_agg(a.attname ORDER BY a.attnum)
-        FROM unnest(c.conkey) AS colnum
-        JOIN pg_attribute a ON a.attrelid = t.oid AND a.attnum = colnum
-      ) = ARRAY['fish_code']
-  LOOP
-    EXECUTE format('ALTER TABLE public.fish DROP CONSTRAINT %I', r.conname);
-  END LOOP;
-END$$;
+-- no-op: superseded by 20251015_000001d_fish_unique_cleanup_fix.sql
+DO $$ BEGIN /* noop */ END $$;
