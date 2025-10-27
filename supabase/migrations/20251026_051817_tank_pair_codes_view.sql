c8618dd86ef3e8001834d69f3e84caca5827c5ca
davekokel <dave.kokel@gmail.com>
2025-10-27 14:41:01 -0700

    migrations: bootstrap v_tank_pairs with NULL placeholders (no joins); later migrations overwrite

--- supabase/migrations/20251026_051817_tank_pair_codes_view.sql
+++ supabase/migrations/20251026_051817_tank_pair_codes_view.sql
@@ -1,4 +1,4 @@
--- Bootstrap: drop dependents then create minimal v_tank_pairs
+-- Bootstrap: drop dependents, create minimal v_tank_pairs (later files overwrite)
 DROP VIEW IF EXISTS public.cross_clutch_instances;
 DROP VIEW IF EXISTS public.v_cross_clutch_instances;
 DROP VIEW IF EXISTS public.v_tank_pairs;
@@ -6,10 +6,9 @@ DROP VIEW IF EXISTS public.v_tank_pairs;
 CREATE VIEW public.v_tank_pairs AS
 SELECT
   tp.tank_pair_code::text AS tank_pair_code,
-  fp.fish_pair_code::text AS fish_pair_code,
-  fp.mom_fish_code::text  AS mom_fish_code,
-  fp.dad_fish_code::text  AS dad_fish_code,
+  NULL::text              AS fish_pair_code,
+  NULL::text              AS mom_fish_code,
+  NULL::text              AS dad_fish_code,
   NULL::text              AS mother_tank_code,
   NULL::text              AS father_tank_code
-FROM public.tank_pairs tp
-LEFT JOIN public.fish_pairs fp ON fp.fish_pair_code = tp.fish_pair_code;
+FROM public.tank_pairs tp;
