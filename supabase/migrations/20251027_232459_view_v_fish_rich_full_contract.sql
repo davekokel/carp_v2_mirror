8a4401f9e444e4b4f98266ed98a4ffa2834d9299
davekokel <dave.kokel@gmail.com>
2025-10-27 16:46:00 -0700

    noop: neutralize 232459; later migrations define canonical v_fish_rich

--- supabase/migrations/20251027_232459_view_v_fish_rich_full_contract.sql
+++ supabase/migrations/20251027_232459_view_v_fish_rich_full_contract.sql
@@ -1,16 +1,1 @@
-CREATE OR REPLACE VIEW public.v_fish_rich AS
-SELECT
-  f.fish_uuid::uuid         AS fish_uuid,
-  f.fish_code::text         AS fish_code,
-  NULL::text                AS fish_name,
-  NULL::text                AS fish_nickname,
-  NULL::text                AS genetic_background,
-  NULL::text                AS line_building_stage,
-  NULL::date                AS date_birth,
-  0::int                    AS allele_number,
-  NULL::text                AS allele_code,
-  0::int                    AS n_active_tanks,
-  NULL::text                AS transgene_pretty,
-  NULL::text                AS genotype_rollup,
-  f.created_at::timestamptz AS created_at
-FROM public.fish f;
+DO $$ BEGIN NULL; END $$;
