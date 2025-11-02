dd21e2ac5dd958fcd0fe6df966fff720f026f9e3
davekokel <dave.kokel@gmail.com>
2025-11-02 13:49:22 -0800

    chore(migrations): neutralize legacy temp-view migrations to unblock rebuild

---
+++ supabase/migrations/20251102_142000_vci_derive_clutch_genotype_codes.sql
@@ -0,0 +1,5 @@
+BEGIN;
+DO $$ BEGIN
+  RAISE NOTICE 'Skipping legacy temp-view migration 20251102_142000_vci_derive_clutch_genotype_codes.sql';
+END $$;
+COMMIT;
---
+++ supabase/migrations/20251102_143000_vci_derive_clutch_genotype_codes_fix.sql
@@ -0,0 +1,5 @@
+BEGIN;
+DO $$ BEGIN
+  RAISE NOTICE 'Skipping legacy temp-view migration 20251102_143000_vci_derive_clutch_genotype_codes_fix.sql';
+END $$;
+COMMIT;
